b1e04c06a15913026988bc3a4ff26774b5a34aa7
Ivo Ventura <65545951+venturaivo@users.noreply.github.com>
2024-03-13 14:59:55 +0000

    Add files via upload
    Números pares em Python

---
+++ 07.py
@@ -0,0 +1,14 @@
+
+pares = 0
+impares = 0
+
+for i in range(10):
+  numero = int(input("Insira um número: "))
+
+  if numero % 2 == 0:
+    pares += 1
+  else:
+    impares += 1
+
+print(f"Pares: {pares}")
+print(f"Ímpares: {impares}")
